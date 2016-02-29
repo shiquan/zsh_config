eeef415bd86200049267131383b70fdac67c8b30
Shi Quan <shiquan.cn@gmail.com>
2016-02-29 13:56:34 +0800

    modified:   lib/aliases.zsh

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,5 +1,6 @@
 alias p='ps -HAcl -F S -A f'
 alias sw='du -h --max-depth=1 | sort -n -r'
-alias e='emacs -nw'
+alias -g emacs='/usr/local/bin/emacs'
+alias e='/usr/local/bin/emacs -nw'
 alias -g ls='ls -GF'
-
+alias -g la='less -S'

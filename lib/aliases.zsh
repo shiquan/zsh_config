ed58d24299f7b68d114cabddef64fdeb57d08278
Shi Quan <shiquan.cn@gmail.com>
2016-05-12 13:12:31 +0800

    modified:   lib/aliases.zsh 	modified:   lib/env.zsh

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,6 +1,6 @@
 alias p='ps -HAcl -F S -A f'
 alias sw='du -h --max-depth=1 | sort -n -r'
-alias -g emacs='/usr/local/bin/emacs'
+#alias -g emacs='/usr/local/bin/emacs'
 alias e='/usr/local/bin/emacs -nw'
 alias -g ls='ls -GF'
 alias -g la='less -S'
--- lib/env.zsh
+++ lib/env.zsh
@@ -1,1 +1,1 @@
-export PATH=/usr/local/texlive/2015/bin/x86_64-darwin:$PATH
+export PATH=/usr/local/bin/:/usr/local/texlive/2015/bin/x86_64-darwin:$PATH
